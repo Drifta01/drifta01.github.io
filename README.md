31159a544e69f5cf6052c041da7aa737e986b374
Firstname Lastname <carlguilford@gmail.com>
2024-07-10 04:59:30 +1200

    0907

---
+++ README.md
@@ -0,0 +1,2 @@
+# carl-g-blog<zero-md src="https://raw.githubusercontent.com/maxpowernz/vanillahtml/main/md/1.md">
+
---
+++ js/links.js
@@ -0,0 +1,76 @@
+const links = [
+  {
+    title: "Tic Tac Toe",
+    fileName:"/tic-tac-toe.html",
+    type: "html",
+    order: 9,
+    linkText: "Tic Tac Toe",
+  },
+  {
+    title: "en of sprint 5",
+    fileName: "posts/end-of-sprint-5.md",
+    type: "md",
+    order: 1,
+    linkText: "end-of-sprint-5",
+  },
+  {
+    title: "reflect",
+    fileName:"foundations-and-reflelections.html",
+    type: "html",
+    order: 0,
+    linkText: "Foundations",
+  },
+  {
+    title: "identity",
+    fileName: "/blogpages/learning-plan.html",
+    type: "html",
+    order: 2,
+    linkText: "Learning Plan",
+  },
+  {
+    title: "java cafe",
+    fileName: "/blogpages/java-cafe.html",
+    type: "html",
+    order: 7,
+    linkText: "Java Cafe",
+  },
+  {
+    title: "classes",
+    fileName: "posts/md/classes.md",
+    type: "md",
+    order: 3,
+    linkText: "classes & ids",
+  },
+  {
+    title: "te houtaewa",
+    fileName: "/blogpages/te-houtaewa.html",
+    type: "html",
+    order: 4,
+    linkText: "te-houtaewa",
+  },
+  {
+    title: "index",
+    fileName: "index.html",
+    type: "html",
+    order: 5,
+    linkText: "home",
+  },
+  {
+    title: "Problem Solving",
+    fileName: "/blogpages/problem-solving.html",
+    type: "html",
+    order: 6,
+    linkText: "Problem Solving",
+  },
+  {
+    title: "DOM",
+    fileName: "/posts/DOM-applying-classes/dom.html",
+    type: "html",
+    order: 7,
+    linkText: "The DOM",
+  },
+];
+
+
+
+export default links;
