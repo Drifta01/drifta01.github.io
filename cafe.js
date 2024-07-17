e352d02fe854c9c2189d9dd82ac370a917ed7680
Firstname Lastname <carlguilford@gmail.com>
2024-07-17 16:54:55 +1200

    tic-tac-fix

--- cafe.js
+++ cafe.js
@@ -1,36 +1,22 @@
 // Stock Control
 let products = {
-  whiteCoffee: { stock: 5,
-     price: 4 },
+  whiteCoffee: { stock: 5, price: 4 },
   blackCoffee: { stock: 5, price: 3 },
   sandwich: { stock: 9, price: 3 },
   muffin: { stock: 2, price: 3.5 },
   eggs: { stock: 1, price: 3.5 },
 };
 
-function displayProducts() {
-  document.getElementById("whiteCoffee").innerHTML = "white Coffee :" + products.whiteCoffee.stock;
-
-  document.getElementById("blackCoffee").innerHTML = "Black Coffee : " + products.blackCoffee.stock;
-
-  document.getElementById("sandwich").innerHTML = "sandwich :" + products.sandwich.stock;
-
-  document.getElementById("muffin").innerHTML = "muffin :" + products.muffin.stock;
-
-  document.getElementById("eggs").innerHTML = "Eggs" + products.eggs.stock;
-  displayProducts();
-}
-
 // Display Products
 function displayProducts() {
   for (const [key, value] of Object.entries(products)) {
-    document.getElementById(key).innerHTML =  value.stock;
-    displayProducts();
+    document.getElementById(key).innerHTML = `${key} : ${value.stock}`;
   }
 }
+displayProducts();
 
 // Customer Object
-let customer = { order: [0] };
+let customer = { order: [] };
 let minOrderSize = 1;
 let maxOrderSize = 5;
 
@@ -45,12 +31,14 @@ function generateCustomerOrder() {
     let productName = productNames[productIndex];
     newOrder.push(productName);
   }
+
   customer.order = newOrder;
+  displayCustomerOrder();
 }
 
 // Display Customer Order
 function displayCustomerOrder() {
-  document.getElementById("customerOrder").innerHTML = "Customer order: " + customer.order.join(", ");
+  document.getElementById("customerOrder").innerHTML = `Customer order: ${customer.order.join(", ")}`;
 }
 document.getElementById("customerButton").onclick = generateCustomerOrder;
 
@@ -58,37 +46,42 @@ document.getElementById("customerButton").onclick = generateCustomerOrder;
 let cash = 0;
 
 function displayCash() {
-  document.getElementById("cash").innerHTML = "Cash"  + cash ;
-
-  displayCash();
+  document.getElementById("cash").innerHTML = `Cash: ${cash}`;
 }
-document.getElementById("fillOrder").onclick = fillOrder;
+displayCash();
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 function fillOrder() {
   let saleTotal = 0;
-
-  let alertBox = document.getElementById("alertBox");
 
   customer.order.forEach((productName) => {
     if (products[productName].stock > 0) {
       products[productName].stock--;
       saleTotal += products[productName].price;
     } else {
-      alertBox.innerHTML = "I'm sorry, we're out of " + productName ;
-      displayAlert()
+      alert(`I'm sorry, we're out of ${productName}`);
     }
   });
+  displayAlert();
 
   cash += saleTotal;
   customer.order = [];
-  displayCustomerOrder()
+
   displayProducts();
   displayCash();
-  displayFillOrder()
+  displayCustomerOrder();
 }
+
+document.getElementById("fillOrder").onclick = fillOrder;
+
+// Clear Menu
+function clearMenu() {
+  customer.order = [];
+  displayCustomerOrder();
+}
+
+document.getElementById("clearMenu").onclick = clearMenu;
+
+// Helper Function: Generate Random Number Within Range
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
