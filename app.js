4cc9cf09b1d5e17ae0f63e4b34fa0db2ac25a092
Drifta01 <carlguilford@gmail.com>
2025-07-20 06:19:48 +1200

    123

--- app.js
+++ app.js
@@ -5,24 +5,31 @@ const postElements = document.querySelectorAll(".post");
 async function readDirectoryAndOutputHTML(owner, repo, path) {
   const octokit = new Octokit({});
   try {
-    const response = await octokit.request(`GET /repos/${owner}/${repo}/contents/${path}`, {
-      owner,
-      repo,
-      path,
-      accept: "application/vnd.github.html+json",
-    });
+    const response = await octokit.request(
+      `GET /repos/${owner}/${repo}/contents/${path}`,
+      {
+        owner,
+        repo,
+        path,
+        accept: "application/vnd.github.html+json",
+      }
+    );
     console.log(response);
     response.data.forEach(async (element, index) => {
       if (element.type === "file") {
-        const fileResponse = await octokit.request(`GET /repos/${owner}/${repo}/git/blobs/${element.sha}`, {
-          owner,
-          repo,
-          accept: "application/vnd.github.html+json",
-        });
+        const fileResponse = await octokit.request(
+          `GET /repos/${owner}/${repo}/git/blobs/${element.sha}`,
+          {
+            owner,
+            repo,
+            accept: "application/vnd.github.html+json",
+          }
+        );
         console.log(fileResponse);
         const data = await fileResponse.data;
         const decoded = atob(data.content);
-        document.getElementById("post-content").innerHTML = marked.parse(decoded);
+        document.getElementById("post-content").innerHTML =
+          marked.parse(decoded);
       }
     });
   } catch (error) {
@@ -35,7 +42,8 @@ function getPost(file) {
   const postContentWrapper = document.getElementById("zd-wrapper");
   const postContent = document.getElementById("zd-content");
   const section = document.getElementById("section");
-  const iframe = document.getElementById("iframe"); const loader = document.getElementById("loader-wrapper");
+  const iframe = document.getElementById("iframe");
+  const loader = document.getElementById("loader-wrapper");
 
   // loader.classList.remove("hide");
   // postContentWrapper.classList.add("hide");
@@ -60,8 +68,7 @@ function getPost(file) {
     section.classList.add("hide");
     iframe.classList.add("hide");
     postContentWrapper.classList.remove("hide");
-      postContent.src = file.fileName;
-
+    postContent.src = file.fileName;
   }
 }
 
